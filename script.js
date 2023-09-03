4870206ffa04e3f6e24211d2673717dd662302a3
skolky <97928223+skolky@users.noreply.github.com>
2023-09-03 07:44:15 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+const sound = new Audio();
 console.log("Hello World");
 
 function func(){
-    alert("Thank you for joining Shabbhirism.")
+    sound.src = "sound.mp3";
+    sound.play();
+    confirm("Are you sure that you want to join shabbhirism?");
 }
 
-document.getElementById("button").addEventListener("click", func)
+
+
+document.getElementById("button").addEventListener("click", func);
+
